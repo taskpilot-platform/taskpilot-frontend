b752af87c567062a6809e6241c4fa5b7e6b3327a
Thien P. Dang <23521476@gm.uit.edu.vn>
2026-02-22 08:29:15 +0000

    Add Docker configuration and Nginx setup for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +9,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  server: {
+    host: true, // host: true tương đương với việc luôn gắn cờ --host
+    port: 5173, // (Tùy chọn) Chốt luôn cổng 5173 cho chắc chắn
+  },
+});
